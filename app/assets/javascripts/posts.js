391f10cc57a508079d94e875070f81ad16872bec
Topher Hunt <hunt.topher@gmail.com>
2016-10-30 16:47:35 -0500

    Lighter styling of Post bottom comments

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,20 +1,29 @@
 $(function(){
 
+  $('.this-comment .js-show-on-hover').hide();
+  $('.this-comment').hover(function(){
+    $(this).find('.js-show-on-hover').stop().fadeIn(200);
+  }, function(){
+    $(this).find('.js-show-on-hover').stop().fadeOut(200);
+  });
+
   // JS-disabled functionality isn't a priority right now.
   $('.js-new-reply').click(function(e){
     e.preventDefault();
     var link = $(this);
     var parent_id = link.data('post-id');
+    var loading_indicator = link.siblings('.loading');
+    var new_reply_div = $('.new-reply-container[data-parent-id="'+parent_id+'"]');
     link.hide();
-    link.siblings('.loading').show();
+    loading_indicator.show();
 
     $.ajax({
       url: '/posts.js?parent_id='+parent_id,
       method: 'POST',
       dataType: 'json',
       success: function(data){
-        link.siblings('.new-reply-container').show().html(data.form_html);
-        link.siblings('.loading').hide();
+        new_reply_div.show().html(data.form_html);
+        loading_indicator.hide();
       },
       error: function(){
         alert("Error loading this post. Please refresh your browser page.");
@@ -25,12 +34,12 @@ $(function(){
   $('.js-edit-reply').click(function(e){
     e.preventDefault();
     var link = $(this);
-    var container = link.parent().parent();
-    var parent_id = link.data('post-id');
+    var container = link.parents('.bottom-comment').first();
     link.hide();
     link.siblings('.loading').show();
 
-    container.load('/posts/' + container.data('id') + '/edit.js');
+    container.children('.this-comment').load('/posts/' + container.data('id') + '/edit.js');
     // The partial should init autosave once it's loaded
   });
+
 });
