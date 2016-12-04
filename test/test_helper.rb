2c003b88913aa73e4d907b3c661ba272c5644195
Topher Hunt <hunt.topher@gmail.com>
2016-12-04 17:22:03 -0600

    Monkeypatch Minitest to print full backtrace on assertion failure

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,19 @@ require "support/factories"
 require 'support/global_helpers'
 require 'support/controller_helpers'
 require 'support/feature_helpers'
+
+# Monkeypatch Minitest to provide full backtrace on assertion failures
+# instead of the default 1 line (which strongly discourages using helper methods)
+module Minitest
+  module Assertions
+    def assert test, msg = nil
+      self.assertions += 1
+      unless test then
+        msg ||= "Expected #{mu_pp test} to be truthy."
+        msg = msg.call if Proc === msg
+        raise "Assertion failed.\n  #{msg}"
+      end
+      true
+    end # def
+  end # module
+end # module
