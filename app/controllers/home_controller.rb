13e0bd9f43a7b7a562be68004575817cfd0d9354
Topher Hunt <hunt.topher@gmail.com>
2016-12-04 18:07:23 -0600

    About page displays info about each team member

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   end
 
   def about
+    @team = User.where(id: ENV.fetch('TEAM_USER_IDS').split(',')).to_a.shuffle
   end
 
   def throwup
--- test/support/factories.rb
+++ test/support/factories.rb
@@ -5,6 +5,17 @@ FactoryGirl.define do
     email { "#{full_name.downcase.gsub(/[^\w]+/, '_')}@example.com" }
     password              "password"
     password_confirmation "password"
+    # These are overkill for most test users, but useful in the dev environment
+    self_dreams              { Faker::Lorem.sentence }
+    self_passions            { Faker::Lorem.sentence }
+    self_skills              { Faker::Lorem.sentence }
+    self_proud_traits        { Faker::Lorem.sentence }
+    self_weaknesses          { Faker::Lorem.sentence }
+    self_evolve              { Faker::Lorem.sentence }
+    self_looking_for         { Faker::Lorem.sentence }
+    self_work_at             { Faker::Lorem.sentence }
+    self_professional_goals  { Faker::Lorem.sentence }
+    self_fields_of_expertise { Faker::Lorem.sentence }
     confirmed_at 1.week.ago
   end
 
