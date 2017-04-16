f8d6a864d28b5ff9b90865f7b8ea4e8e925033a7
Topher Hunt <hunt.topher@gmail.com>
2017-04-15 22:33:19 -0500

    Troubleshoot Heroku email sending problems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV.fetch("HOSTNAME") }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
     address:   ENV.fetch("SMTP_HOST"),
