0ea5042407c50cd24e8efa367fbe78b829224ed8
Topher Hunt <hunt.topher@gmail.com>
2017-05-31 11:53:08 +0200

    Condense like buttons etc.

--- test/features/get_involved_flags_test.rb
+++ test/features/get_involved_flags_test.rb
@@ -13,12 +13,9 @@ class GetInvolvedFlagsTest < Capybara::Rails::TestCase
     assert_equals 0, @user.created_get_involved_flags.count
     page.find('.get-involved a').click
     assert_selector '.get-involved a.active'
-    assert_selector '.get-involved-flags-count'
     assert_equals 1, @user.created_get_involved_flags.count
     assert_equals @project, @user.created_get_involved_flags.first.target
     page.find('.get-involved a').click
     assert_equals 0, @user.created_get_involved_flags.count
-    refute_selector '.get-involved-flags-count'
-    refute_selector '.get-involved-flags-details' # non-owner can't view details
   end
 end
--- test/features/stay_informed_flags_test.rb
+++ test/features/stay_informed_flags_test.rb
@@ -13,12 +13,9 @@ class StayInformedFlagsTest < Capybara::Rails::TestCase
     assert_equals 0, @user.created_stay_informed_flags.count
     page.find('.stay-informed a').click
     assert_selector '.stay-informed a.active'
-    assert_selector '.stay-informed-flags-count'
     assert_equals 1, @user.created_stay_informed_flags.count
     assert_equals @project, @user.created_stay_informed_flags.first.target
     page.find('.stay-informed a').click
     assert_equals 0, @user.created_stay_informed_flags.count
-    refute_selector '.stay-informed-flags-count'
-    refute_selector '.stay-informed-flags-details' # non-owner can't view details
   end
 end
