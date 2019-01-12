ea2cef78241faa463e8e12135d88d4aee9e6dd17
Topher Hunt <hunt.topher@gmail.com>
2019-01-12 14:37:58 +0100

    Trivial commit to test Unicorn shutdown behavior

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,6 @@ before_fork do |server, worker|
     puts "Unicorn master intercepting TERM and sending myself QUIT instead."
     Process.kill 'QUIT', Process.pid
   end
-
   ActiveRecord::Base.connection.disconnect!
 end
 
@@ -17,6 +16,5 @@ after_fork do |server, worker|
   Signal.trap('TERM') do
     puts "Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT."
   end
-
   ActiveRecord::Base.establish_connection
 end
